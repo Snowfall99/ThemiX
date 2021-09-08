d304379433c47b2caefba1a310e94a268b9e594d
Snowfall99 <445117445@qq.com>
2021-09-08 14:18:03 +0800

    update address and remove log_filter

--- themix/client.sh
+++ themix/client.sh
@@ -1,9 +1,5 @@
 echo request number = $1
-for count in {1..100}
-do
-    ./single.sh $1 http://127.0.0.1:11200/client &
-    ./single.sh $1 http://127.0.0.1:11210/client &
-    ./single.sh $1 http://127.0.0.1:11220/client &
-    ./single.sh $1 http://127.0.0.1:11230/client &
-    echo $count
-done
+./single.sh $1 http://127.0.0.1:11200/client &
+./single.sh $1 http://127.0.0.1:11210/client &
+./single.sh $1 http://127.0.0.1:11220/client &
+./single.sh $1 http://127.0.0.1:11230/client &
