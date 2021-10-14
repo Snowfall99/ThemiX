31ec64ed85a08116a1c678e3b0fc356a53b48a2b
Snowfall99 <445117445@qq.com>
2021-10-14 09:39:25 +0800

    update client.c

--- themix/client.c
+++ themix/client.c
@@ -3,8 +3,14 @@
 #include <curl/curl.h>
 #include <sys/time.h>
 
+void curl_init(CURL *curl, char *addr, char* payload) {
+    curl_easy_setopt(curl, CURLOPT_URL, addr);
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, payload);
+}
+
 int main(int argc, char** argv) {
     int requests = atoi(argv[1]);
+    printf("requets: %d\n", requests);
     CURL *curl0, *curl1, *curl2, *curl3, *curl4;
     CURLcode res;
     time_t start;
@@ -13,30 +19,70 @@ int main(int argc, char** argv) {
     FILE *fp = NULL;
     fp = fopen("coordinator.txt", "w+");
     if (!fp) {
-        printf("open file error");
+        printf("open file error\n");
         return 0;
     }
     
-    curl_global_init(CURL_GLOBAL_ALL);
+    // curl_global_init(CURL_GLOBAL_ALL);
+    // CURLM* curlm = curl_multi_init();
 
     curl0 = curl_easy_init();
+    if (curl0) {
+        curl_init(curl0, "http://localhost:11200/client", "a");
+        // curl_multi_add_handle(curlm, curl0);
+    }
     curl1 = curl_easy_init();
+    if (curl1) {
+        curl_init(curl1, "http://localhost:11210/client", "a");
+        // curl_multi_add_handle(curlm, curl1);
+    }
     curl2 = curl_easy_init();
+    if (curl2) {
+        curl_init(curl2, "http://localhost:11220/client", "a");
+        // curl_multi_add_handle(curlm, curl2);
+    }
     curl3 = curl_easy_init();
+    if (curl3) {
+        curl_init(curl3, "http://localhost:11230/client", "a");
+        // curl_multi_add_handle(curlm, curl3);
+    }
     curl4 = curl_easy_init();
-    
-    if (curl0 && curl1 && curl2 && curl3 && curl4) {
-        curl_easy_setopt(curl0, CURLOPT_URL, "http://localhost:11200/client");
-        curl_easy_setopt(curl0, CURLOPT_POSTFIELDS, "a");
-        curl_easy_setopt(curl1, CURLOPT_URL, "http://localhost:11210/client");
-        curl_easy_setopt(curl1, CURLOPT_POSTFIELDS, "a");
-        curl_easy_setopt(curl2, CURLOPT_URL, "http://localhost:11220/client");
-        curl_easy_setopt(curl2, CURLOPT_POSTFIELDS, "a");
-        curl_easy_setopt(curl3, CURLOPT_URL, "http://localhost:11230/client");
-        curl_easy_setopt(curl3, CURLOPT_POSTFIELDS, "a");
-        curl_easy_setopt(curl4, CURLOPT_URL, "http://localhost:11240/client");
-        curl_easy_setopt(curl4, CURLOPT_POSTFIELDS, "a");
+    if (curl4) {
+        curl_init(curl4, "http://localhost:11240/client", "a");
+        // curl_multi_add_handle(curlm, curl4);
+    }
 
+    // time(&start);
+    // for (int i = 0; i < requests; i++) {
+    //     struct timeval tv;
+    //     char start_str[100], end_str[100];
+    //     gettimeofday(&tv, NULL);
+    //     sprintf(start_str, "%d start: %ld", i, (tv.tv_sec*1000 + tv.tv_usec/1000));
+    //     fprintf(fp, "%s\n", start_str);
+    //     int running_handlers = 0;
+    //     do {
+    //         curl_multi_wait(curlm, NULL, 0, 2000, NULL);
+    //         curl_multi_perform(curlm, &running_handlers);
+    //     } while (running_handlers > 0);
+    //     gettimeofday(&tv, NULL);
+    //     sprintf(end_str, "%d end: %ld", i, (tv.tv_sec*1000 + tv.tv_usec/1000));
+    //     fprintf(fp, "%s\n", end_str);
+    // }
+    // time(&end);
+
+    // printf("duration is %ld seconds\n", (end - start));
+
+
+    // curl_easy_cleanup(curl0);
+    // curl_easy_cleanup(curl1);
+    // curl_easy_cleanup(curl2);
+    // curl_easy_cleanup(curl3);
+    // curl_easy_cleanup(curl4);
+
+    // curl_multi_cleanup(curlm);
+
+
+    if (curl0 && curl1 && curl2 && curl3 && curl4) {
         time(&start);
         for (int i = 0; i < requests; i++) {
             struct timeval tv;
