888a503c6e6e58eec180e655d205e5acb528084f
Snowfall99 <445117445@qq.com>
2021-11-18 19:10:29 +0800

    themix echo stable

--- themix/server/propose.go
+++ themix/server/propose.go
@@ -44,12 +44,12 @@ func initProposer(lg *zap.Logger, tp transport.Transport, id info.IDType, reqc c
 }
 
 func (proposer *Proposer) proceed(seq uint64) {
-	proposer.lock.Lock()
-	defer proposer.lock.Unlock()
+	// proposer.lock.Lock()
+	// defer proposer.lock.Unlock()
 
-	if proposer.seq <= seq {
-		proposer.reqc <- []byte{} // insert an empty reqeust
-	}
+	// if proposer.seq <= seq {
+	// 	proposer.reqc <- []byte{} // insert an empty reqeust
+	// }
 }
 
 func (proposer *Proposer) run() {
